90fcecd23cb57619efa06f9984781572d9bfd31d
Jacob Joaquin <jacobjoaquin@gmai.com>
2018-05-18 22:17:22 +0000

    Added notes to omxplaye_test.py

--- scripts/omxplayer_test.py
+++ scripts/omxplayer_test.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 
+'''omxplayer_test.py
+
+Modified example from:
+http://python-omxplayer-wrapper.readthedocs.io/en/latest/
+'''
+
 from omxplayer.player import OMXPlayer
 from pathlib import Path
 from time import sleep
 
 VIDEO_PATH = Path("/home/pi/Documents/disorient-tv/DTV-Pornj-Walk-With-Me.mov")
-
 player = OMXPlayer(VIDEO_PATH)
-
 sleep(5)
-
 player.quit()
