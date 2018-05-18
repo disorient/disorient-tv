42668b16d216602e3f19ee4ce02e78c37cfd2f4a
Jacob Joaquin <jacobjoaquin@gmai.com>
2018-05-18 23:39:58 +0000

    Added example playing two movies back to back.

---
+++ scripts/two_movies.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+'''Play two files
+
+Example of playing two files back to back with duration.
+'''
+
+from omxplayer.player import OMXPlayer
+from pathlib import Path
+from time import sleep
+
+def play_it(file):
+    path = Path(file)
+    player = OMXPlayer(path)
+    sleep(player.duration())
+
+play_it("/home/pi/Documents/disorient-tv/DTV-Pornj-Walk-With-Me.mov")
+play_it("/home/pi/Documents/disorient-tv/DTV-Mono-Skull.mov")
+player.quit()
